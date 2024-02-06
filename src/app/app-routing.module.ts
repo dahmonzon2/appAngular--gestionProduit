a7d54bab7aa186cbf790653e405f8dbfee4ed27d
dahmonzon2 <you@example.com>
2024-02-06 22:44:14 +0100

    modification page connexion/inscription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProduitsComponent } from './produits/produits.component';
 import { AddProduitComponent } from './add-produit/add-produit.component';
 import { UpdateProduitComponent } from './update-produit/update-produit.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 const routes: Routes = [
   {path:"produits", component: ProduitsComponent},
   {path:"add-produit", component: AddProduitComponent},
   {path:"", redirectTo:"produits", pathMatch:"full"},
-  {path:"updateProduit/:id",component:UpdateProduitComponent}
+  {path:"updateProduit/:id",component:UpdateProduitComponent},
+  {path:"connexion", component: ConnexionComponent},
+  {path:"inscription", component: InscriptionComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AddProduitComponent } from './add-produit/add-produit.component';
 import { FormsModule } from '@angular/forms';
 import { UpdateProduitComponent } from './update-produit/update-produit.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitsComponent,
     AddProduitComponent,
-    UpdateProduitComponent
+    UpdateProduitComponent,
+    ConnexionComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
